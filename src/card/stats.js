bf3d3eb033bedfa2db38e9bc4cfa3700033347c0
ysogg <ajb.personal@hotmail.com>
2025-09-13 13:10:53 -0400

    typo

--- src/card/stats.js
+++ src/card/stats.js
@@ -1,7 +1,7 @@
 const statsCard = (stats, options ={}) => {
   return `
-    <svg width="300" height="130" xlmns="http://www.w3.org/2000/svg>
-      <rect width="200" height="100" x="10" y="10" rx="2" ry="20" fill="bblue" />
+    <svg width="300" height="130" xlmns="http://www.w3.org/2000/svg">
+      <rect width="200" height="100" x="10" y="10" rx="2" ry="20" fill="blue" />
     </svg>
   `
 }
