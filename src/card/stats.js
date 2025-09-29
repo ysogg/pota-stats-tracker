89099b7617d8bb259fd2703df848c3682a580293
ysogg <ajb.personal@hotmail.com>
2025-09-28 20:30:40 -0400

    gross but general idea for now
    Need this to be properly dynamic. Also need to specify the column category (activator/hunter).

--- src/card/stats.js
+++ src/card/stats.js
@@ -1,6 +1,30 @@
 import { Card } from "../components/Card.js"
 
+const createTextNode = ({
+  index,
+  name,
+  activatorValue,
+  hunterValue,
+}) => {
+  const delay = (index + 3) * 150;
 
+  return `
+    <g class="stagger" style="animation-delay: ${delay}ms" transform="translate(25,0)">
+      <text x="0" y="${45 + index * 20}">${name}</text>
+      <text x="100" y="${45 + index * 20}">${activatorValue}</text>
+      <text x="200" y="${45 + index * 20}">${hunterValue}</text>
+    </g>
+  `;
+}
+
+const getStyles =() => {
+  return `
+    .stagger {
+      opacity: 100;
+      animation: fadeInAnimation 0.3s ease-in-out forwards;
+    }
+  `;
+}
 
 const renderStatsCard = (statsobj, options ={}) => {
   const {
@@ -26,34 +50,53 @@ const renderStatsCard = (statsobj, options ={}) => {
   STATS.activatorParks = {
     value: stats.activator.parks,
     id: "activatorParks",
-  }
+  };
+
+  STATS.hunterParks = {
+    value: stats.hunter.parks,
+    id: "hunterParks",
+  };
 
   STATS.activatorQSOs = {
     value: stats.activator.qsos,
     id: "activatorQSOs",
-  }
+  };
 
-  //const statItems = Object.keys(STATS).map((key, index) => 
-    //createTextNode({
-      //value: STATS[key].value,
-      //id: STATS[id].id,
-      //bold: text_bold,
-    //})
-  //)
+  STATS.hunterQSOs = {
+    value: stats.activator.qsos,
+    id: "activatorQSOs",
+  };
+/*
+  for (let i = 0; i < 3; i++) {
+    switch(i) {
+      case 0:
+        createTextNode(index: 0, name: "Activations", activatorValue: STATS.activatorActivations, hunterValue: STATS.hunterActivations);
+        break;
+      case 1:
+        createTextNode(index: 1, name: "Parks", activatorValue: STATS.activatorParks, hunterValue: STATS.hunterParks);
+        break;
+      case 2:
+        createTextNode(index: 2, name: "QSOs", activatorValue: STATS.activatorQSOs, hunterValue: STATS.hunterQSOs);
+        break;
+    }
+  }
+*/
+
+  const cssStyles = getStyles();
 
   const card = new Card({
+    width: 467,
+    height: 170,
     title: callsign,
   });
 
+  card.setCSS(cssStyles);
+
   return card.render(`
     <svg x="0" y="0">
-      <g>
-        <text x="20" y="30">${STATS.callsign.value}</text>
-        <text x="20" y="45">Activator:</text>
-        <text x="25" y="60">${STATS.activatorActivations.value}</text>
-        <text x="25" y="75">${STATS.activatorParks.value}</text>
-        <text x="25" y="90">${STATS.activatorQSOs.value}</text>
-      </g>
+        ${createTextNode({index: 0, name: "Activations", activatorValue: STATS.activatorActivations.value, hunterValue: "---"})}
+        ${createTextNode({index: 1, name: "Parks", activatorValue: STATS.activatorParks.value, hunterValue: STATS.hunterParks.value})}
+        ${createTextNode({index: 2, name: "QSOs", activatorValue: STATS.activatorQSOs.value, hunterValue: STATS.hunterQSOs.value})}
     </svg>
     `);
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,25 @@
 class Card {
   constructor({
+    width = 100,
+    height = 100,
     title,
   }) {
+    this.width = width;
+    this.height = height;
     this.title = title;
+
+    this.css = "";
+  }
+
+  setCSS(value) {
+    this.css = value;
   }
 
   render(body) {
     return `
       <svg
-        width="300"
-        height="130"
+        width="${this.width}"
+        height="${this.height}"
         xmlns="http://www.w3.org/2000/svg"
         role="img"
       >
@@ -18,17 +28,21 @@ class Card {
           .header {
             font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
           }
+          ${this.css}
         </style>
         <rect
-          width="200"
-          height="100"
-          x="10"
-          y="10"
-          rx="20"
-          ry="20"
+          width="${this.width - 1}"
+          height="99%"
+          x="0.5"
+          y="0.5"
+          rx="10"
+          ry="10"
           stroke="black"
           fill="white"
         />
+
+        <text x="20" y="30" class="header">${this.title}</text>
+
         <g>
           ${body}
         </g>
